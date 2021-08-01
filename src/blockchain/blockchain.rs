555c9b1009ceeae73e90bd2a66e3d048571abd36
Tejas Mehta <tmthecoder@gmail.com>
2021-08-01 12:36:39 -0500

    add blockchain and worldstate

--- src/blockchain/blockchain.rs
+++ src/blockchain/blockchain.rs
@@ -1,1 +1,12 @@
-pub struxt
+/// The whole blockchain
+pub struct Blockchain {
+    /// All of the accepted or valid blocks in the chain
+    pub blocks: Vec<Block>,
+
+    /// Map containing accounts with their correlating accountId
+    /// Also represents the WorldState
+    pub accounts: HashMap<String, Account>,
+
+    /// Stores transactions that haven't been added to the chain yet
+    pending_transactions: Vec<Transaction>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/// A rust blockchain demo
+/// Made following the article here:
+/// https://hackernoon.com/rusty-chains-a-basic-blockchain-implementation-written-in-pure-rust-gk2m3uri
+
 fn main() {
     println!("Hello, world!");
 }
