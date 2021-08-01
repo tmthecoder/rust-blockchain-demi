954433425711ea064bdd098a810cf68ee5662fe2
Tejas Mehta <tmthecoder@gmail.com>
2021-08-01 12:57:32 -0500

    create the account struct

--- src/blockchain/account.rs
+++ src/blockchain/account.rs
@@ -1,3 +1,12 @@
+/// An account on the Blockchain
+/// The basic part of the world state
 pub struct Account {
-    
+    /// A key value store for the account
+    store: HashMap<String, String>,
+
+    /// The account type (user or something else)
+    acc_type: AccountType,
+
+    /// Amount of tokens the account holds
+    tokens: u128,
 }
--- src/blockchain/transaction.rs
+++ src/blockchain/transaction.rs
@@ -2,12 +2,16 @@
 pub struct Transaction {
     /// A unique number (prevents replay attacks)
     nonce: u128,
+
     /// Account ID of the caller
     from: String,
+
     /// Timestamp
     created_at: SystemTime,
+
     /// Type of transaction and additional info
     pub(crate) record: TransactionData,
+
     /// Signature of the message
     signature: Option<String>
 }
@@ -17,10 +21,13 @@ pub struct Transaction {
 pub enum TransactionData {
     /// Create a new user account
     CreateUserAccount(String),
+
     /// Change any stored value in an account
     ChangeStoreValue{key: String, value: String},
+
     /// Move tokens from one account to another
     TransferTokens{to: String, amount: u128},
+
     /// Create tokens out of the blue
     CreateTokens{receiver: String, amount: u128}
     // Add more as needed
